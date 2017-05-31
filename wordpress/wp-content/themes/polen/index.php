0956f9ec763e606f076c5a0de041e71b7f8aefcc
rruellepetel <ruellepetelromain@gmail.com>
2017-05-31 14:50:36 +0200

    imporve index.php

--- wordpress/wp-content/themes/polen/index.php
+++ wordpress/wp-content/themes/polen/index.php
@@ -7,16 +7,37 @@ get_header()
  <div class="container">
   <div class="row">
     <div class="col-md-12">
+      <?php echo do_shortcode("[metaslider id=4]"); ?>
+      <img src="<?php echo get_template_directory_uri()?>/static/images/bandeau-blanc.png" alt="">
         <?php
             // while(have_posts()):
             //     the_post();
             //     the_content();
 
-    echo do_shortcode("[metaslider id=4]");
 
 
             // endwhile
                          ?>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <img src="<?php echo get_template_directory_uri()?>/static/images/pep8.png" alt="">
+        <h4>Pépinière d'entreprises</h4>
+        <p>Hébergement des entreprises innovantes du département</p>
+        <a href="#">Infos</a>
+      </div>
+      <div class="col-md-4">
+        <img src="<?php echo get_template_directory_uri()?>/static/images/tic8.png" alt="">
+        <h4>Centre de ressources TIC</h4>
+        <p>Un espace dédié aux nouvelles technologies pour les entreprises</p>
+      <a href="#">Infos</a>
+      </div>
+      <div class="col-md-4">
+        <img src="<?php echo get_template_directory_uri()?>/static/images/tele8.png" alt="">
+        <h4>Le Télécentre SoLozère</h4>
+        <p>Un équipement au services des télétravailleurs</p>
+        <a href="#">Infos</a>
+      </div>
     </div>
 <div class="row">
   <h2>Inscription à la Newsletter</h2>
