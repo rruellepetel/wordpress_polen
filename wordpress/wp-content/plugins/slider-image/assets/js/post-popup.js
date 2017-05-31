ddb604e6e5f659e41092b084f7f2418f485a3335
rruellepetel <ruellepetelromain@gmail.com>
2017-05-31 14:10:11 +0200

    add newsletter

--- wordpress/wp-content/plugins/slider-image/assets/js/post-popup.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery('#hugeit_slider_insert_slider_to_post').on('click', function() {
-		if ((window.parent.tinyMCE || window.parent.tinyMCE.activeEditor) && jQuery.isNumeric(jQuery(this).siblings('select').val())) {
-			window.parent.send_to_editor('[huge_it_slider id="' + jQuery(this).siblings('select').val() + '"]');
-		}
-	});
-});
--- wordpress/wp-content/themes/polen/index.php
+++ wordpress/wp-content/themes/polen/index.php
@@ -7,9 +7,24 @@ get_header()
  <div class="container">
   <div class="row">
     <div class="col-md-12">
-      <?php echo do_shortcode("[huge_it_slider id='1']"); ?>
-    </div>
+        <?php
+            // while(have_posts()):
+            //     the_post();
+            //     the_content();
 
+    echo do_shortcode("[metaslider id=4]");
+
+            // endwhile
+                         ?>
+    </div>
+<div class="row">
+  <h2>Inscription à la Newsletter</h2>
+  <input type="text" name="" value="">
+  <button type="submit" name="button">Envoyer</button>
+  <h2>Agenda</h2>
+  <p>Aucun Evenement</p>
+  <span class="glyphicon glyphicon-bitcoin" aria-hidden="true"></span>
+</div>
   </div>
 
 
